cb4cff613e4c3e3241ff7f53d4924f0a72b91a89
polinsky96 <janpolin96@gmail.com>
2022-10-12 13:50:09 +0300

    fixed hidden main content

--- src/viewportHeightCalc.js
+++
@@ -1,8 +0,0 @@
-export default function (){
-    function setHeight() {
-      var vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-    }
-    setHeight();
-    window.addEventListener('resize', setHeight);
-}
